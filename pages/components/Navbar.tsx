bdabf3b3b556d7ada22db9314b6a849c3fdffb73
Leandro Koller <lekoller@yahoo.com>
2022-03-13 15:52:19 -0300

    feat: Add authentication icon.

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,52 +1,25 @@
 import React, { useContext } from "react";
 import Image from "next/image";
-import { createStyles, makeStyles } from "@mui/styles";
 import { Button } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 import logo from "../../assets/logo.svg";
-import styles from "../../styles/Home.module.css";
-import theme from "../../styles/theme";
+import styles from "../../styles/Navbar.module.css";
 import { AuthContext } from "../../contexts/AuthContext";
 import axios from "axios";
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    title: {
-      font: theme.typography.fontFamily,
-      color: theme.palette.grey[800],
-      fontWeight: 500,
-      fontSize: "36px",
-    },
-    titleSpan: {
-      color: theme.palette.grey[700],
-      fontWeight: 300,
-    },
-    button: {
-      backgroundColor: theme.palette.primary.main,
-      color: "white",
-      width: "173px",
-      height: "48px",
-      boxSizing: "border-box",
-      textTransform: "capitalize",
-      fontWeight: 300,
-      letterSpacing: ".06rem",
-    },
-  })
-);
-
 function Navbar() {
-  const classes = useStyles();
-  const { setToken } = useContext(AuthContext);
+  const { setToken, token } = useContext(AuthContext);
   const authURL = "https://localhost:7098/api/home/auth";
 
   const simulateAuthentication = async () => {
     const response = await axios({
       method: "post",
       url: authURL,
-    })
+    });
 
     setToken(response.data);
-  }
+  };
 
   return (
     <nav className={styles.nav}>
@@ -54,13 +27,18 @@ function Navbar() {
         <Image src={logo} alt="wa-project logo" />
       </div>
       <div>
-        <h1 className={classes.title}>
-          WarriorSales<span className={classes.titleSpan}>API</span>
+        <h1 className={styles.title}>
+          WarriorSales<span className={styles.titleSpan}>API</span>
         </h1>
       </div>
-      <div>
-        <Button 
-          className={styles.button} 
+      <div className={styles.auth}>
+        <CheckCircleOutlineIcon
+          fontSize="large"
+          color="primary"
+          className={token ? styles.icon : styles.iconInvisible}
+        />
+        <Button
+          className={styles.button}
           variant="contained"
           onClick={simulateAuthentication}
         >
